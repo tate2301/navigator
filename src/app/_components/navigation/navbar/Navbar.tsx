49447b920283009951cb613351c3ef3b90b2b293
tatendabakozw <tatendabakozw@gmail.com>
2024-01-16 00:30:00 +0200

    fix: navbar link home

--- src/app/_components/navigation/navbar/Navbar.tsx
+++ src/app/_components/navigation/navbar/Navbar.tsx
@@ -1,9 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 
 const Navbar = () => {
   return (
-    <div className='max-w-7xl w-full mx-auto px-4'>Navbar</div>
+    <div className='max-w-7xl w-full mx-auto px-4 flex flex-row items-center'>
+      <p className='flex-1'>Navbar</p>
+      <Link href={'/dashboard'}>
+        Dashboard
+      </Link>
+    </div>
   )
 }
 
